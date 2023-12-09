f974addd8d975d13a9dc4471482063b988f89f9a
hunien <huniens06@gmail.com>
2023-12-09 17:58:22 +0700

    new repository

--- Bank13.java
+++ Bank13.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class Bank13 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input13 = new Scanner(System.in);
         int init_sav_amount, sav_period;
         double final_sav_amount_, interest, interest_percent= 0.02;
         System.out.print("Input your initial savings amount: ");
-        init_sav_amount = sc.nextInt();
+        init_sav_amount = input13.nextInt();
         System.out.print("Input your savings period: ");
-        sav_period = sc.nextInt();
+        sav_period = input13.nextInt();
         interest = sav_period *interest_percent *init_sav_amount;
         final_sav_amount_= interest + init_sav_amount;
         System.out.println("Interest: " + interest);
